bb2cd2964a1772d351d5a9b1d627b98c94325779
Trung Hieu <167515573+dempseyyindahouse@users.noreply.github.com>
2024-12-03 13:14:09 +0700

    Track.java

--- dsai/aims/Track.java
+++ dsai/aims/Track.java
@@ -1,1 +1,30 @@
+package lab02.hust.soict.dai.aims.media;
 
+public class Track implements Playable {
+    private String title;
+    private int length;
+
+    public Track(String title, int length) {
+        this.title = title;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void play() {
+        System.out.println("Playing track: " + this.getTitle());
+        System.out.println("Track length: " + this.getLength());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) return true; //kiem tra tham chieu
+      
+    }
+}
