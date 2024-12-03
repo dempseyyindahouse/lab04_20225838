3054981bbbdcae67f66aa3a9e1d26ac740af0c7d
Trung Hieu <167515573+dempseyyindahouse@users.noreply.github.com>
2024-12-03 13:30:17 +0700

    Update Aims.java

--- main/lab02/Aims/Aims.java
+++ main/lab02/Aims/Aims.java
@@ -1,17 +1,53 @@
-public class Aims {
-    public static void main(String[] args) {
-      Cart myCart = new Cart();
-      DigitalVideoDisc dvd2 = new DigitalVideoDisc("Sasuke", "Anime", "Kishimoto", 20, 8.5f);
-      DigitalVideoDisc dvd3 = new DigitalVideoDisc("Vegeta", "Action", "Toriyama", 22, 9.0f);
-      DigitalVideoDisc dvd1 = new DigitalVideoDisc("One Piece", "Anime", "Oda", 20, 8.0f);
-  
-      myCart.addDVD(dvd1);
-      myCart.addDVD(dvd2);
-      myCart.addDVD(dvd3);
-  
-      myCart.print();
-  
-      myCart.removeDVD(dvd3);
-      myCart.print();
+//Phan Thanh Thang
+//20225927
+//Class "Cart" source code
+package Lab02.Aims.Cart;
+
+import Lab02.hust.soict.dsai.aims.media.Media;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED = 20;	
+	private ArrayList<Media> itemsOrdered = new ArrayList<Media>();
+    // Constructor không tham số
+    public Cart() {
+        this.itemsOrdered = new ArrayList<Media>();
     }
-  }
+    
+	public void addMedia(Media media) {
+	    if (!itemsOrdered.contains(media)) {
+	        itemsOrdered.add(media);
+	        System.out.println(media.getTitle() + " has been added to the cart.");
+	    } else {
+	        System.out.println(media.getTitle() + " is already in the cart.");
+	    }
+	}
+
+	public void removeMedia(Media media) {
+	    if (itemsOrdered.contains(media)) {
+	        itemsOrdered.remove(media);
+	        System.out.println(media.getTitle() + " has been removed from the cart.");
+	    } else {
+	        System.out.println(media.getTitle() + " is not in the cart.");
+	    }
+	}
+	
+	public float totalCost() {
+	    float total = 0;
+	    for (Media media : itemsOrdered) {
+	        total += media.getCost();
+	    }
+	    return total;
+	}
+	
+    public void sortMediaByTitleCost() {
+        Collections.sort(itemsOrdered, Media.COMPARE_BY_TITLE_COST);
+    }
+
+    public void sortMediaByCostTitle() {
+        Collections.sort(itemsOrdered, Media.COMPARE_BY_COST_TITLE);
+    }
+    
+}
