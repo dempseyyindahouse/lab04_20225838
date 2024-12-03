fcd659a6d8b27f2c31737112076c58a9d58feb0e
Trung Hieu <167515573+dempseyyindahouse@users.noreply.github.com>
2024-12-03 12:46:23 +0700

    book.java

--- dsai/aims/book.java
+++ dsai/aims/book.java
@@ -1,1 +1,26 @@
+public class Book {
+    private String title;
+    private String category;
+    private float cost;
+    private String[] authors = new ArrayList<String>();
 
+    // Phương thức thêm tác giả
+    public void addAuthor(String authorName) {
+        if (authors.contains(authorName)) {
+            System.out.println("Author \"" + authorName + "\" is already in the list.");
+        } else {
+            authors.add(authorName);
+            System.out.println("Author \"" + authorName + "\" added.");
+        }
+    }
+
+    // Phương thức xóa tác giả
+    public void removeAuthor(String authorName) {
+        if (authors.contains(authorName)) {
+            authors.remove(authorName);
+            System.out.println("Author \"" + authorName + "\" removed.");
+        } else {
+            System.out.println("Author \"" + authorName + "\" not found in the list.");
+        }
+    }
+}
