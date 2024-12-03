76492e0567936b48e12e688f7cf5bb54d9da4805
Trung Hieu <167515573+dempseyyindahouse@users.noreply.github.com>
2024-12-03 13:21:24 +0700

    Track.java

--- dsai/aims/Track.java
+++ dsai/aims/Track.java
@@ -1,30 +1,36 @@
-package lab02.hust.soict.dai.aims.media;
+package Lab02.hust.soict.dsai.aims.media;
 
 public class Track implements Playable {
-    private String title;
-    private int length;
+	
+	private String title;
+	private int length;
+	
+	public Track(String title, int length) {
 
-    public Track(String title, int length) {
-        this.title = title;
-        this.length = length;
-    }
+		this.title = title;
+		this.length = length;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public int getLength() {
-        return length;
-    }
-
-    public void play() {
+	public int getLength() {
+		return length;
+	}
+	
+	public void play() {
         System.out.println("Playing track: " + this.getTitle());
         System.out.println("Track length: " + this.getLength());
-    }
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+	    if (this == obj) return true; // Kiểm tra tham chiếu
+	    if (obj == null || getClass() != obj.getClass()) return false; // Kiểm tra kiểu đối tượng
 
-    @Override
-    public boolean equals(Object obj) {
-      if (this == obj) return true; //kiem tra tham chieu
-      
-    }
+	    Track track = (Track) obj; // Ép kiểu
+	    return this.title.equals(track.title) && this.length == track.length; // So sánh title và length
+	}
+
 }
