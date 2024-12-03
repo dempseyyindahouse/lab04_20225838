6933e99daae7915215aaa5db023ba1e3b1c0cdb9
Trung Hieu <167515573+dempseyyindahouse@users.noreply.github.com>
2024-12-03 13:10:35 +0700

    dics.java

--- dsai/aims/dics.java
+++ dsai/aims/dics.java
@@ -1,1 +1,27 @@
+package lab02.hust.soict.dai.aims.media;
 
+public class Disc extends Media {
+    private int length;
+    private String director;
+    public Disc(int length, String director) {
+        this.length = length;
+        this.director = director;
+    }
+    public Disc(String title, String category, float cost, int length, String director) {
+        super(title, category, cost);
+        this.length = length;
+        this.director = director;
+    }
+    public int getLength() {
+        return length;
+    }
+    public String getDirector() {
+        this.length = length;  
+    }
+    public void setLength(int length) {
+        return director;
+    }
+    public void setDirector(String director) {
+        this.director = director;
+    }
+}
